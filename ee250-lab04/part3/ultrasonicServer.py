686e87e2fd245af86e957649dfb9c7dcca1c58af
Hsin Pan <hsin>
2018-02-09 17:53:28 -0800

    change port to 9000

--- ee250-lab04/part3/ultrasonicServer.py
+++ ee250-lab04/part3/ultrasonicServer.py
@@ -10,14 +10,14 @@ def Process1():
     # Change the host and port as needed. For ports, use a number in the 9000 
     # range. 
     host = '10.0.2.15'
-    port = 9003
+    port = 9000
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((host,port))
 
     print("Process 1 Server Started")
     while True:
-        data, addr = s.recvfrom(1500)
+        data, addr = s.recvfrom(1024)
         data = data.decode('utf-8')
         print("Message From: " + str(addr))
         print("From connected user: " + data)
