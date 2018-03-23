7bd0e12b379de11ff3c8d3ed369bd55a3986c4b2
Hsin Pan <hsin>
2018-03-23 14:48:30 -0700

    a

--- ee250/lab07/vm_subscriber.py
+++ ee250/lab07/vm_subscriber.py
@@ -11,7 +11,6 @@ def on_connect(client, userdata, flags, rc):
     #subscribe to the ultrasonic ranger topic here
     client.subscribe("anrg-pi14/ultrasonicRanger")
     client.subscribe("anrg-pi14/button")
-    client.message_callback_add("anrg-pi14/led", led_callback)
     client.message_callback_add("anrg-pi14/button", button_callback)
 
 
